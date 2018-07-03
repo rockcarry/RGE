c50fe98d491a0478a5ad5294140b191ed91c6088
chenk <chenk@apical.com.cn>
2018-07-03 17:32:46 +0800

    修改 pal 相关代码，解决 PALRGE 的 bug

--- utils.c
+++ utils.c
@@ -40,133 +40,6 @@ void palutils_alpha_palmap(BYTE *palmap, BYTE *pal, DWORD color)
     }
 }
 
-/* ++ palutils_matchpal */
-/* 内部类型定义 */
-typedef struct {
-    int  color;
-    long freq;
-} COLORITEM;
-
-static int color_item_cmp(const void *a, const void *b)
-{
-    COLORITEM *item1 = (COLORITEM*)a;
-    COLORITEM *item2 = (COLORITEM*)b;
-    if      (item2->freq - item1->freq > 0) return  1;
-    else if (item2->freq - item1->freq < 0) return -1;
-    else    return 0;
-}
-
-/* 函数实现 */
-void palutils_matchpal(BMP *pb1, BMP *pb2)
-{
-    COLORITEM colors[512];
-    BYTE      newpal[256*4];
-    BYTE      cmap1 [256] = {0};
-    BYTE      cmap2 [256] = {0};
-    BYTE      *ppal   = NULL;
-    BYTE      *pixel  = NULL;
-    int        color;
-    int        i, j;
-    int        black1;
-    int        black2;
-
-    /* 该函数仅用于 256 色调色板位图 */
-    if (!pb1->ppal || !pb2->ppal) return;
-
-    /* 初始化颜色统计表 */
-    for (i=0; i<512; i++) {
-        colors[i].color = i;
-        colors[i].freq  = 0;
-    }
-
-    /* 统计颜色分布 */
-    for (i=0; i<pb1->stride*pb1->height; i++) {
-        colors[*(pb1->pdata + i) + 0  ].freq++;
-    }
-    for (i=0; i<pb2->stride*pb2->height; i++) {
-        colors[*(pb2->pdata + i) + 256].freq++;
-    }
-
-    /* 保证 0 号调色板为黑色 */
-    black1 = PALRGB(pb1->ppal, 0, 0, 0);
-    black2 = PALRGB(pb2->ppal, 0, 0, 0);
-    if (colors[black1].freq > colors[black2 + 256].freq) {
-        colors[black1 + 0  ].freq = 0x3fffffffL; /* 不能使用 0x7fffffff */
-        colors[black1 + 256].freq = 0;           /* 否则在下一步合并重复颜色时会溢出 */
-    } else {
-        colors[black1 + 0  ].freq = 0;
-        colors[black2 + 256].freq = 0x3fffffffL;
-    }
-
-    /* 合并重复颜色 */
-    for (i=0; i<256; i++) {
-        for (j=0; j<256; j++) {
-            if (  pb1->ppal[i*4+0] == pb2->ppal[j*4+0]
-               && pb1->ppal[i*4+1] == pb2->ppal[j*4+1]
-               && pb1->ppal[i*4+2] == pb2->ppal[j*4+2] )
-            {
-                colors[i].freq += colors[j+256].freq;
-                colors[j+256].freq = 0;
-            }
-        }
-    }
-
-    /* 对颜色表进行排序 */
-    qsort(colors, 512, sizeof(COLORITEM), color_item_cmp);
-
-    /* 生成新调色板并建立匹配的映射 */
-    for (i=0; i<256; i++) {
-        color = colors[i].color;
-        if (color < 256) {
-            ppal = pb1->ppal;
-            cmap1[color] = i;
-        } else {
-            ppal   = pb2->ppal;
-            color -= 256;
-            cmap2[color] = i;
-        }
-
-        newpal[i * 4 + 0] = ppal[color * 4 + 0];
-        newpal[i * 4 + 1] = ppal[color * 4 + 1];
-        newpal[i * 4 + 2] = ppal[color * 4 + 2];
-    }
-
-    /* 建立未匹配的映射 */
-    for (i=0; i<256; i++) {
-        if (cmap1[i] == 0) {
-            cmap1[i] = PALRGB(newpal,
-                pb1->ppal[i * 4 + 2],
-                pb1->ppal[i * 4 + 1],
-                pb1->ppal[i * 4 + 0]);
-        }
-
-        if (cmap2[i] == 0) {
-            cmap2[i] = PALRGB(newpal,
-                pb2->ppal[i * 4 + 2],
-                pb2->ppal[i * 4 + 1],
-                pb2->ppal[i * 4 + 0]);
-        }
-    }
-
-    /* 修改图像数据 */
-    pixel = pb1->pdata;
-    for (i=0; i<pb1->stride * pb1->height; i++) {
-        *pixel = cmap1[*pixel];
-         pixel++;
-    }
-
-    pixel = pb2->pdata;
-    for (i=0; i<pb2->stride * pb2->height; i++) {
-        *pixel = cmap2[*pixel];
-         pixel++;
-    }
-
-    /* 修改调色板 */
-    setbmppal(pb1, 0, 256, newpal);
-    setbmppal(pb2, 0, 256, newpal);
-}
-/* -- palutils_matchpal */
-
 #else
 /* 在这里书写测试程序 */
 #include <stdlib.h>
@@ -177,59 +50,39 @@ void palutils_matchpal(BMP *pb1, BMP *pb2)
 #include "font.h"
 #include "utils.h"
 
-static BMP mybmp1 = {0};
-static BMP mybmp2 = {0};
-
 void main(void)
 {
     void *ctxt;
+    BMP   mybmp1 = {0};
     BYTE  palmap1[256];
     BYTE  palmap2[256];
 
     createbmp(&SCREEN);
 
-    loadfont(&FONT16);
-    loadbmp(&mybmp1, "res/me.bmp"    , NULL);
-    loadbmp(&mybmp2, "res/boy332.bmp", NULL);
+    loadfont(&FONT12);
+    loadbmp(&mybmp1, "res/me.bmp", NULL);
 
     palutils_bitmap_palmap(palmap1, SCREEN.ppal, mybmp1.ppal);
     palutils_alpha_palmap (palmap2, SCREEN.ppal, ARGB(128, 0, 0, 255));
 
     ctxt = draw2d_init(&SCREEN);
-    settextfont (ctxt, &FONT16);
+    settextfont (ctxt, &FONT12);
     settextcolor(ctxt, RGB888(0, 255, 0));
     paint_begin (ctxt);
     printtext   (ctxt, "调色板映射演示 + 调色板匹配演示");
     paint_done  (ctxt);
     draw2d_free (ctxt);
 
-    putbmp(&SCREEN, 0 , 20, &mybmp1, FS_256_PALMAPSRC, 0, 0, palmap1);
+    putbmp(&SCREEN, 0 , 15, &mybmp1, FS_256_PALMAPSRC, 0, 0, palmap1);
     putbmp(&SCREEN, 60, 60, &mybmp1, FS_256_PALMAPDST, 0, 0, palmap2);
-    getch();
-
-    putbmp(&SCREEN, 0 , 20, &mybmp1, FS_SOLID, 0, 0, NULL);
-    putbmp(&SCREEN, 60, 60, &mybmp1, FS_SOLID, 0, 0, NULL);
-    setbmppal(&SCREEN, 0, 256, mybmp1.ppal);
-    putbmp(&SCREEN, 0, 20, &mybmp1, FS_AUTO_LOCK|FS_256_COPYDATA, 0, 0, NULL);
-    getch();
-
-    setbmppal(&SCREEN, 0, 256, mybmp2.ppal);
-    putbmp(&SCREEN, 150, 20, &mybmp2, FS_AUTO_LOCK|FS_256_COPYDATA, 0, 0, NULL);
-    getch();
-
-    palutils_matchpal(&mybmp1, &mybmp2);
-    setbmppal(&SCREEN, 0, 256, mybmp1.ppal);
-    putbmp(&SCREEN, 0  , 20, &mybmp1, FS_AUTO_LOCK|FS_256_COPYDATA, 0, 0, NULL);
-    putbmp(&SCREEN, 150, 20, &mybmp2, FS_AUTO_LOCK|FS_256_COPYDATA, 0, 0, NULL);
     getch();
 
     savebmp(&SCREEN, "screen.bmp", NULL);
     getch();
 
     destroybmp(&mybmp1);
-    destroybmp(&mybmp2);
     destroybmp(&SCREEN);
-    freefont(&FONT16);
+    freefont(&FONT12);
 }
 #endif
 
